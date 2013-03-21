9dd52cf7a35eae332524e7f66a990ef67ba388bd
Pete Kazmier <pete@kazmier.com>
2013-03-20 21:41:04 -0400

    First version of the Philips Hue Lua API.
    I'm a Lua newbie so I still haven't figured out how to best package
    this as a library for the community, but for now, at least the code is
    online. This includes a simple Hue API (hue.lua) and a simple command
    line tool (huectl) that can be used to control your Philips Hue
    lights.

---
+++ hue.lua
@@ -0,0 +1,161 @@
+--- Lua Hue API
+--
+-- Copyright Pete Kazmier 2013
+
+local M = {}
+
+local json = require 'json'
+local http = require 'socket.http'
+local ltn12 = require 'ltn12'
+local seq = require 'pl.seq'
+local tablex = require 'pl.tablex'
+local stringx = require 'pl.stringx'
+
+
+--- Discovers any local Hue bridges on the network
+-- @return a list of IP addresses
+function M.discover()
+   local r = M.jsonRequest('http://www.meethue.com/api/nupnp')
+   if r then 
+      return tablex.imap(function(b) return b.internalipaddress end, r)
+   end
+end
+
+-- Bridge object definition
+
+M.Bridge = {}
+
+--- Creates a new bridge object. Upon creation, the bridge is queried
+-- for the valid lights that are registered to it.  
+-- @param host the IP address of the bridge
+-- @param username the username to authenticate with
+-- @param o optional table containing initial state
+-- @return instantiated Bridge object
+function M.Bridge:new(host, username, o)
+   assert(host,'hostname is nil')
+   assert(username,'username is nil') 
+   o = o or {}
+   o.host = host
+   o.username = username
+   setmetatable(o, self)
+   self.__index = self
+   o:lights()
+   return o
+end
+
+--- Returns the list of lights registered with the bridge.
+-- @param no_cache if true, do not return previously cached results
+-- @return array of lights
+function M.Bridge:lights(no_cache)
+   if not no_cache and self.ids then
+      return self.ids,self.names
+   end
+
+   local r = self:request('/lights')
+   if not r then 
+      return nil 
+   end
+
+   self.ids,self.names = {}, {}
+   for k,v in pairs(r) do
+      self.ids[k] = v.name
+      self.names[v.name] = k
+   end
+   return self.ids,self.names
+end
+
+--- Returns the id of the specified light
+-- @param light a string representing the id or name assigned to a light
+-- @return the id as a string or nil if the light does not exist
+function M.Bridge:lookup_id(light)
+   return self.ids[light] and light or self.names[light]
+end
+
+--- Sets the state of the specified lights. State is specified as a table
+-- of items specified from http://developers.meethue.com/1_lightsapi.html.
+-- @param lights a list of lights specified by id or name
+-- @param state a table containing the desired state
+-- @return a table showing success or error for each attribute set
+function M.Bridge:set_state(lights, state)
+   resources,invalid = self:resource_uris(lights, '/state', '/action')
+   local results = {}
+   for _,uri in ipairs(resources) do 
+      results[uri] = self:request(uri,'PUT',state)
+   end
+   return results
+end
+
+--- Gets the state of the specified lights. State is specified as a table
+-- of items specified from http://developers.meethue.com/1_lightsapi.html.
+-- @param lights a list of lights specified by id or name
+-- @param state a table containing the desired state
+-- @return a table showing status of specified resources
+function M.Bridge:get_state(lights)
+   resources,invalid = self:resource_uris(lights)
+   local results = {}
+   for _,uri in ipairs(resources) do 
+      results[uri] = self:request(uri)
+   end
+   return results
+end
+
+function M.Bridge:resource_uris(lights, light_suffix, group_suffix)
+   light_suffix = light_suffix or ''
+   group_suffix = group_suffix or ''
+
+   if #lights == 0 then 
+      return {'/groups/0'..group_suffix}, {}
+   end
+
+   local resources,invalid = {},{}
+   for _,l in ipairs(lights) do
+      local id = self:lookup_id(l)
+      if id then 
+         resources[#resources+1]='/lights/'..id..light_suffix
+      else
+         invalid[#invalid+1]=l
+      end
+   end
+   return resources,invalid
+end
+
+function M.Bridge:request(resource, method, body)
+   resource = resource or '/'
+
+   local url = 'http://'..self.host..'/api/'..self.username..resource
+
+   return M.jsonRequest(url, method, body)
+end
+
+-- Utility functions
+
+function M.jsonRequest(url, method, body)
+   assert(url)
+   method = method or 'GET'
+
+   if body then
+      body = json.encode(body)
+   end
+
+   local results = {}
+   local _, code = http.request {
+      url = url,
+      method = method,
+      headers = { ['content-type']='application/json', 
+                  ['content-length']=body and #body or 0},
+      source = ltn12.source.string(body),
+      sink = ltn12.sink.table(results),
+   }
+
+   if code < 200 or code > 299 then
+      return nil, code
+   end
+
+   results = table.concat(results)
+   return json.decode(results), code
+end
+
+
+-- End of module
+
+return M
